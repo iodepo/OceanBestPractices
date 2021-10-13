31eb6ea7a5e2e7daad50a4fa64360ef18b59d3df
Marc Huffnagle <marc@element84.com>
2021-10-13 17:02:24 -0400

    Add a "Marc" stage for development

--- bin/ocean-best-practices.ts
+++ bin/ocean-best-practices.ts
@@ -3,12 +3,18 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import StatefulApiPipelineStack from '../cdk/stateful-api-pipeline-stack';
+import StatefulApiStage from '../cdk/stateful-api-stage';
 
 const app = new cdk.App();
 
 new StatefulApiPipelineStack(app, 'StatefulApiPipelineStack', {
   env: { region: 'us-east-1' },
   description: 'Pipeline for managing deployments of the stateful api stack'
+});
+
+new StatefulApiStage(app, 'MarcStatefulApiStage', {
+  env: { region: 'us-east-1' },
+  stage: 'marc'
 });
 
 app.synth();
