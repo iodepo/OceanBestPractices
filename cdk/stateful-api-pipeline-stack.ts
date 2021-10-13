0dbe263c77abc78575c1ceec4c39459ca6487fce
Marc Huffnagle <marc@element84.com>
2021-10-13 16:37:32 -0400

    Add development stage

--- cdk/stateful-api-pipeline-stack.ts
+++ cdk/stateful-api-pipeline-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from "@aws-cdk/core";
 import { Construct } from "constructs";
 import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
 import { GitHubTrigger } from "@aws-cdk/aws-codepipeline-actions";
+import StatefulApiStage from "./stateful-api-stage";
 
 export default class StatefulApiPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
@@ -14,6 +15,11 @@ export default class StatefulApiPipelineStack extends Stack {
         }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
-    })
+    });
+
+    pipeline.addStage(new StatefulApiStage(this, 'Development', {
+      env: { region: 'us-east-1' },
+      stage: 'development'
+    }));
   }
 }
