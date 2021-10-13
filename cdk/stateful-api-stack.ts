2f853097446cf17c594b7f4824ea514e26a95204
Marc Huffnagle <marc@element84.com>
2021-10-13 17:19:54 -0400

    Add ES domain to stateful API

--- cdk/stateful-api-stack.ts
+++ cdk/stateful-api-stack.ts
@@ -1,19 +1,29 @@
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
-import * as opensearch from '@aws-cdk/aws-opensearchservice';
+import * as es from '@aws-cdk/aws-elasticsearch';
+import { ElasticsearchVersion } from "@aws-cdk/aws-elasticsearch";
 
 interface StatefulApiStackProps extends StackProps {
   stage: string
 }
 
 export default class StatefulApiStack extends Stack {
-  public readonly opensearchDomain: opensearch.Domain;
+  public readonly elasticsearchDomain: es.IDomain;
 
   constructor(scope: Construct, id: string, props: StatefulApiStackProps) {
-    const {
-      stage,
-      ...superProps
-    } = props;
+    const { stage, ...superProps } = props;
 
     super(scope, id, superProps);
+
+    this.elasticsearchDomain = new es.Domain(this, 'Elasticsearch', {
+      domainName: `obp-cdk-${stage}`,
+      version: ElasticsearchVersion.V6_0,
+      capacity: {
+        dataNodeInstanceType: 't2.small.elasticsearch',
+        dataNodes: 1
+      },
+      advancedOptions: {
+        'rest.action.multi.allow_explicit_index': 'true'
+      }
+    });
   }
 }
