2283b72a8fe395fde384926b3f58d015520d889b
Ron Propri <rpropri@element84.com>
2021-11-10 11:35:56 -0500

    OBP-281: Adds tooltip package, adds temp tooltips to search fields fropdown, cleans up sass files

--- website/src/js/components/DropdownLink.js
+++ website/src/js/components/DropdownLink.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
-const DropdownLink = ({ title, id, onClick }) => {
+const DropdownLink = ({ title, id, help_text, onClick }) => {
 
   function handleClick(e) {
     if ( typeof onClick === 'function' ) {
@@ -10,7 +11,14 @@ const DropdownLink = ({ title, id, onClick }) => {
 
   return (
     <li>
-      <a onClick={handleClick} data-value={id || title}>{ title }</a>
+      <a onClick={handleClick} data-value={id || title} data-tip data-for={id}>
+        {title}
+        {
+          help_text
+          ? <ReactTooltip id={id} place='right' effect='solid' delayShow={300}>{help_text}</ReactTooltip>
+          : null
+        }
+      </a>
     </li>
   );
 
