a871d94e5a8549574e62cb44c1fa3cce15606da4
Paul Pilone <paul@element84.com>
2021-11-19 17:38:47 -0500

    Adds guard in bulk ingester against missing env vars.

--- ingest/lambdas/bulk-ingester.js
+++ ingest/lambdas/bulk-ingester.js
@@ -1,9 +1,18 @@
 // @ts-check
 const bulkIngester = require('../lib/bulk-ingester');
 
-const handler = async () => bulkIngester(
-  process.env.DSPACE_ENDPOINT,
-  process.env.INGEST_TOPIC_ARN
-);
+const handler = async () => {
+  const dspaceEndpoint = process.env.DSPACE_ENDPOINT;
+  const ingestTopicArn = process.env.INGEST_TOPIC_ARN;
+  if (dspaceEndpoint === undefined || ingestTopicArn === undefined) {
+    console.log('WARN: Bulk ingester called without required environment variables.');
+    return;
+  }
+
+  bulkIngester(
+    dspaceEndpoint,
+    ingestTopicArn
+  );
+};
 
 module.exports = handler;
