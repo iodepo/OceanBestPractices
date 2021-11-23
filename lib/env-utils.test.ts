7f7f008041a2fdedba64beaeebe603e3e6c93dc3
Marc Huffnagle <marc@element84.com>
2021-11-23 13:52:31 -0500

    Consolidate env-utils

--- lib/env-utils.test.ts
+++ lib/env-utils.test.ts
@@ -1,7 +1,4 @@
-import {
-  getListFromEnv,
-  getStringFromEnv,
-} from './env-utils';
+import { getBoolFromEnv, getListFromEnv, getStringFromEnv } from './env-utils';
 
 const testVar = 'TEST_VAR';
 
@@ -44,6 +41,55 @@ describe('env-utils', () => {
     it('returns a multi-element list', () => {
       process.env[testVar] = 'as,df';
       expect(getListFromEnv(testVar)).toEqual(['as', 'df']);
+    });
+  });
+
+  describe('getBoolFromEnv', () => {
+    describe('without a default', () => {
+      it('returns an Error if the environment variable is not set', () => {
+        expect(getBoolFromEnv(testVar)).toBeInstanceOf(Error);
+      });
+
+      it('returns an Error if the environment variable is an empty string', () => {
+        process.env[testVar] = '';
+        expect(getBoolFromEnv(testVar)).toBeInstanceOf(Error);
+      });
+
+      it('returns an Error if the environment variable is an invalid value', () => {
+        process.env[testVar] = 'ASDF';
+        expect(getBoolFromEnv(testVar)).toBeInstanceOf(Error);
+      });
+
+      for (const val of ['true', 'True', 'TRUE']) {
+        it(`returns true for "${val}"`, () => {
+          process.env[testVar] = val;
+          expect(getBoolFromEnv(testVar)).toBe(true);
+        });
+      }
+
+      for (const val of ['false', 'False', 'FALSE']) {
+        it(`returns false for "${val}"`, () => {
+          process.env[testVar] = val;
+          expect(getBoolFromEnv(testVar)).toBe(false);
+        });
+      }
+    });
+
+    describe('with a default', () => {
+      it('returns the value from the environment if set', () => {
+        process.env[testVar] = 'true';
+        expect(getBoolFromEnv(testVar)).toBe(true);
+      });
+
+      it('returns the default if the environment variable is not set', () => {
+        expect(getBoolFromEnv(testVar, true)).toBe(true);
+        expect(getBoolFromEnv(testVar, false)).toBe(false);
+      });
+
+      it('returns an Error if the environment variable is an invalid value', () => {
+        process.env[testVar] = 'ASDF';
+        expect(getBoolFromEnv(testVar)).toBeInstanceOf(Error);
+      });
     });
   });
 });
--- lib/env-utils.ts
+++ lib/env-utils.ts
@@ -14,3 +14,23 @@ export const getListFromEnv = (key: string): string[] | Error => {
 
   return value.split(',');
 };
+
+/**
+ * Read a `true` or `false` value from an environment variable, with an optional
+ * default.
+ */
+export const getBoolFromEnv = (key: string, def?: boolean) => {
+  const value = process.env[key];
+  const error = new TypeError(`Invalid boolean for ${key}: "${value}`);
+
+  if (value) {
+    const uppercaseValue = value.toUpperCase();
+
+    if (uppercaseValue === 'TRUE') return true;
+    if (uppercaseValue === 'FALSE') return false;
+
+    return error;
+  }
+
+  return def ?? error;
+};
