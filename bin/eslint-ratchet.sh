d44d09329e55b84cd3a14b2f4f3021d8fbe60949
Marc Huffnagle <marc@element84.com>
2021-11-30 14:43:39 -0500

    Add eslint-ratchet

---
+++ bin/eslint-ratchet.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p tmp
+
+npx eslint . --ext .js,.jsx,.ts,.tsx | tee tmp/eslint.log
+
+PROBLEMS=$(grep -E '^✖ \d+ problems' tmp/eslint.log | awk '{print $2}')
+
+[ -z "$PROBLEMS" ] && exit 0
+
+RATCHET=$(cat .eslint-ratchet)
+
+if [ "$PROBLEMS" -gt "$RATCHET" ]; then
+  echo "eslint problems increased from ${RATCHET} to ${PROBLEMS}." >&2
+  exit 1
+fi
+
+if [ "$PROBLEMS" -lt "$RATCHET" ]; then
+  if [ "$CI" = "true" ]; then
+    echo "eslint problems decreased from ${PROBLEMS} to ${RATCHET}, but .eslint-ratchet was not updated." >&2
+    exit 1
+  else
+    echo "eslint problems decreased from ${PROBLEMS} to ${RATCHET}. Ratcheting down."
+    echo "$PROBLEMS" > .eslint-ratchet
+  fi
+fi
