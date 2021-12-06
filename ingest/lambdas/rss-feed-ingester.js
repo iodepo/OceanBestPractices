5e102a624c6c51d08417d08aa10e9f61214dd377
Marc Huffnagle <marc@element84.com>
2021-12-06 17:37:21 -0500

    Updates from pairing

--- ingest/lambdas/rss-feed-ingester.js
+++ ingest/lambdas/rss-feed-ingester.js
@@ -6,30 +6,6 @@ const ingestQueue = require('../lib/ingest-queue');
 const { getIntFromEnv, getStringFromEnv } = require('../../lib/env-utils');
 
 /**
- * Returns a boolean indicating whether or not we should publish documents
- * from the RSS feed based on when the RSS feed was published and the
- * last time we checked it. Set the `feedReadInterval` to -1 to always
- * pass this test.
- *
- * @param {Date} publishedDate The published date of the RSS feed.
- * @param {number} feedReadInterval The number in seconds since the
- * published RSS feed was last read.
- *
- * @returns {boolean} Whether or not we should publish new documents based
- *          on the last time we checked the RSS feed
- */
-const shouldQueuePublishedDocuments = (publishedDate, feedReadInterval) => {
-  if (feedReadInterval === -1) {
-    return true;
-  }
-
-  // Assuming the last time we checked for an updated feed was feedReadInterval
-  // seconds ago, if the published date is newer than that we return true.
-  const feedReadIntervalDate = new Date(Date.now() - feedReadInterval);
-  return publishedDate >= feedReadIntervalDate;
-};
-
-/**
  * Fetches a DSpace RSS feed and determines whether or not the documents
  * listed in the feed should be queued for ingest. This is determined by
  * checking the published date of the feed, and whether or not it has been
@@ -40,19 +16,23 @@ const handler = async () => {
   const ingestTopicArn = getStringFromEnv('INGEST_TOPIC_ARN');
   const dspaceFeedReadInterval = getIntFromEnv('DSPACE_FEED_READ_INTERVAL', 300);
 
+  const forcePublishing = dspaceFeedReadInterval === -1;
+
   try {
     // Fetch the RSS feed from DSpace.
     const feed = await dspaceClient.getFeed(dspaceEndpoint);
 
     // Parse out the last published date and use it to determine whether or
     // not we should index new documents.
-    const lastPublishedDate = new Date(feed.channel[0].pubDate[0]._);
-    if (!shouldQueuePublishedDocuments(
-      lastPublishedDate,
-      dspaceFeedReadInterval
-    )) {
-      return;
-    }
+    const feedLastPublished = new Date(feed.channel[0].pubDate[0]._);
+
+    const ingesterLastRun = new Date(Date.now() - dspaceFeedReadInterval);
+
+    const feedIsUpdated = feedLastPublished > ingesterLastRun;
+
+    const processFeed = forcePublishing || feedIsUpdated;
+
+    if (!processFeed) return;
 
     // Loop through the feed items and queue them for ingest. In order to do
     // this we have to fetch the metadata for each item based on the
@@ -62,21 +42,20 @@ const handler = async () => {
     await pMap(
       feed.channel[0].item,
       async (feedItem) => {
-        const dspaceItems = await dspaceClient.find(
+        const [dspaceItem] = await dspaceClient.find(
           dspaceEndpoint,
           'dc.identifier.uri',
           feedItem.link[0]
         );
 
-        if (dspaceItems.length > 0) {
-          const dspaceItem = dspaceItems[0];
-          await ingestQueue.queueIngestDocument(
-            dspaceItem.uuid,
-            ingestTopicArn
-          );
+        if (dspaceItem === undefined) return;
 
-          console.log(`INFO: Queued item ${dspaceItem.uuid} for ingest.`);
-        }
+        await ingestQueue.queueIngestDocument(
+          dspaceItem.uuid,
+          ingestTopicArn
+        );
+
+        console.log(`INFO: Queued item ${dspaceItem.uuid} for ingest.`);
       },
       { concurrency: 5 }
     );
