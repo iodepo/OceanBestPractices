b3d45dd2bc2c4e2cc5c461785cff1a6efa31a72d
Marc Huffnagle <marc@element84.com>
2021-12-10 14:38:25 -0500

    Auto-fix linter warnings

--- api/lambdas/search-by-keywords.js
+++ api/lambdas/search-by-keywords.js
@@ -1,6 +1,5 @@
-'use strict';
-
 const AWS = require('aws-sdk');
+
 const creds = new AWS.EnvironmentCredentials('AWS');
 const region = process.env.REGION || 'us-east-1';
 
@@ -8,7 +7,7 @@ const http = require('http');
 
 const esOpts = {
   host: process.env.ELASTIC_SEARCH_HOST,
-  path: "/documents/_search",
+  path: '/documents/_search',
 };
 
 const esEndpoint = new AWS.Endpoint(esOpts.host);
@@ -16,36 +15,49 @@ const esEndpoint = new AWS.Endpoint(esOpts.host);
 const ontOpts = {
   host: process.env.ONTOLOGY_STORE_HOST,
   port: process.env.ONTOLOGY_STORE_PORT,
-  path: "/sparql",
+  path: '/sparql',
 };
 
-const DEFAULT_FROM = 0, DEFAULT_SIZE = 20;
+const DEFAULT_FROM = 0; const
+  DEFAULT_SIZE = 20;
 
 /**
  * This function is responsible for handling a keyword search and returning matching documents.
  */
 exports.handler = (event, context, callback) => {
   const params = event.queryStringParameters;
-  
+
   if (params === undefined || params === null) {
-    callback(null, { statusCode: 200, body: JSON.stringify({}), headers: responseHeaders() });
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({}),
+      headers: responseHeaders(),
+    });
   } else if (params.keywords === undefined || params.keywords === null) {
-    callback(null, { statusCode: 200, body: JSON.stringify({}), headers: responseHeaders() });
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({}),
+      headers: responseHeaders(),
+    });
   } else {
-    var opts = parseParams(params);
+    const opts = parseParams(params);
     if (opts.synonyms) {
       getSynonyms(opts.keywords, callback)
-        .then(function(results) {
-          var keywords = [].concat(opts.keywords);
-          results.forEach(function(r) { keywords = keywords.concat(r); });
+        .then((results) => {
+          let keywords = [opts.keywords].flat();
+          for (const r of results) { keywords = keywords.concat(r); }
           opts.keywords = keywords;
-          
+
           executeSearch(opts, callback);
-        }).catch(function(err) {
-          callback(err, { statusCode: 500, body: JSON.stringify({'err': err}), headers: responseHeaders() });          
-        }); 
+        }).catch((error) => {
+          callback(error, {
+            statusCode: 500,
+            body: JSON.stringify({ err: error }),
+            headers: responseHeaders(),
+          });
+        });
     } else {
-      executeSearch(opts, callback);  
+      executeSearch(opts, callback);
     }
   }
 };
@@ -54,40 +66,52 @@ exports.handler = (event, context, callback) => {
  * Executes an Elasticsearch query with the given search options and notifies the callback function when
  * it completes. The callback function should be the function that ends this Lambda function, so most likely
  * passed directly from the handler.
- * 
+ *
  * @param {object} options An object defining the search options to use when building the search query.
  * @param {function} callback The function to use as a callback when this asynchronous function finishes.
  */
 function executeSearch(options, callback) {
   const searchBody = JSON.stringify(getSearchDocument(options));
-  
+
   const req = getRequest(searchBody);
 
   const signer = new AWS.Signers.V4(req, 'es');
   signer.addAuthorization(creds, new Date());
 
   const client = new AWS.NodeHttpClient();
-  client.handleRequest(req, null, function(httpResp) {
-    var body = '';
-    httpResp.on('data', function(chunk) {
+  client.handleRequest(req, null, (httpResp) => {
+    let body = '';
+    httpResp.on('data', (chunk) => {
       body += chunk;
     });
 
-    httpResp.on('error', function(err) {
-      callback(err, { statusCode: 500, body: JSON.stringify({'err': err}), headers: responseHeaders() });  
+    httpResp.on('error', (err) => {
+      callback(err, {
+        statusCode: 500,
+        body: JSON.stringify({ err }),
+        headers: responseHeaders(),
+      });
     });
 
-    httpResp.on('end', function(chunk) {
-      callback(null, { statusCode: 200, body: body, headers: responseHeaders() });
+    httpResp.on('end', (chunk) => {
+      callback(null, {
+        statusCode: 200,
+        body,
+        headers: responseHeaders(),
+      });
     });
-  }, function(err) {
-    callback(err, { statusCode: 500, body: JSON.stringify({'err': err}), headers: responseHeaders() });
+  }, (err) => {
+    callback(err, {
+      statusCode: 500,
+      body: JSON.stringify({ err }),
+      headers: responseHeaders(),
+    });
   });
 }
 
 /**
- * Parses the event parameters to define search related parameters and default values. 
- * 
+ * Parses the event parameters to define search related parameters and default values.
+ *
  * @param {object} params The parameters provided when invoking the search function.
  *
  * @returns {object} An object containing the parsed parameters.
@@ -102,8 +126,8 @@ function parseParams(params) {
     sort: params.sort === undefined ? [] : params.sort.split(','),
     fields: params.fields === undefined ? queryFields() : params.fields.split(','),
     synonyms: params.synonyms === undefined ? false : params.synonyms,
-    refereed: params.refereed === undefined ? false : params.refereed
-  }
+    refereed: params.refereed === undefined ? false : params.refereed,
+  };
 }
 
 /**
@@ -129,28 +153,28 @@ function getRequest(body) {
  * information. It also builds the query string value based on keywords provided in the `opts` argument.
  *
  * @param {object} opts Search options to include in the search document. At a minimum this object should contain a
- *                      from, size, keywords, terms | termsURI, fields, and whether or not `refereed` should be 
+ *                      from, size, keywords, terms | termsURI, fields, and whether or not `refereed` should be
  *                      checked.
- * 
+ *
  * @returns {object} A search document object that can be used directly by an Elasticsearch search request.
  */
 function getSearchDocument(opts) {
   const searchDoc = {
-      "_source": {
-        "excludes": ["contents"]
+    _source: {
+      excludes: ['contents'],
+    },
+    from: opts.from,
+    size: opts.size,
+    query: buildElasticsearchQuery(opts.keywords, opts.terms, opts.termURIs, opts.fields, opts.refereed),
+    highlight: {
+      fields: {
+        contents: {},
       },
-      "from": opts.from,
-      "size": opts.size,
-      "query": buildElasticsearchQuery(opts.keywords, opts.terms, opts.termURIs, opts.fields, opts.refereed),
-      "highlight": {
-        "fields": {
-          "contents": {}
-        }
-      },
-      "sort": sortQuery(opts.sort),
-    };
+    },
+    sort: sortQuery(opts.sort),
+  };
 
-  console.log("Search document: " + JSON.stringify(searchDoc));
+  console.log(`Search document: ${JSON.stringify(searchDoc)}`);
 
   return searchDoc;
 }
@@ -159,42 +183,44 @@ function getSearchDocument(opts) {
  * This function takes a valid keyword string and formats it specifically for our
  * Elasticsearch query. It's responsible for parsing logical operators and inserting/removing
  * any necessary or unnecessary quotes.
- * 
+ *
  * e.g. "+ocean current" becomes "AND \"ocean current\""
- **/
+ * */
 function formatKeyword(k) {
   // Map the UI operators to ES operators.
-  const opTransforms = { "+": "AND", "-": "NOT" };
-  
+  const opTransforms = {
+    '+': 'AND',
+    '-': 'NOT',
+  };
+
   // Extract the operator from the keyword.
-  var op = '', fk = k;
-  if (Object.keys(opTransforms).includes(fk.substring(0,1))) {
-    op = opTransforms[fk.substring(0, 1)];
+  let op = ''; let
+    fk = k;
+  if (Object.keys(opTransforms).includes(fk.slice(0, 1))) {
+    op = opTransforms[fk.slice(0, 1)];
     fk = fk.substring(1, fk.length);
   }
-  
+
   // Strip all double quotes from the keyword since we're
   // performing a quoted query in ES.
-  fk = fk.replace(/["]/g, "");
-  
+  fk = fk.replace(/"/g, '');
+
   // Optional: try splitting the search term on a space. If it's a multi-
   // word search term we'll append each term as OR'd AND searches.
   const fk_comps = fk.split(' ');
-  const opt_t = fk_comps.map(t => {
-    return `\"${t}\"`;
-  });
-  
+  const opt_t = fk_comps.map((t) => `\"${t}\"`);
+
   console.log(`opt_t:\n${JSON.stringify(opt_t)}`);
-  
+
   // Construct the query for the primary keyword.
-  fk = op + " \"" + fk + "\"";  
+  fk = `${op} "${fk}"`;
 
   // It's a multi-word keyword. Append a grouped AND for each word in the term
   // and boost the original keyword.
   if (opt_t.length > 1) {
     fk = `${fk}^2 OR ( ${opt_t.join(' AND ')} )`;
   }
-  
+
   return fk;
 }
 
@@ -206,123 +232,122 @@ function formatKeyword(k) {
  * @param {array} termURIs A list of term URIs (ontology URIs) that can be used as filters in the query.
  * @param {array} fields An array of field names to be searched against by the query.
  * @param {boolean} refereed Whether or not `refereed` should be used as a filter.
- * 
+ *
  * @returns {object} The query object that can be used in an Elasticsearch search document `query` field.
  */
 function buildElasticsearchQuery(keywords, terms, termURIs, fields, refereed) {
-  var boolQuery = {
-    "must": {
-      "query_string": {
-        "fields": fields,
-        "query": keywords.length > 0 ? keywords.map(formatKeyword).join(" ") : "*"
-      }
-    }
+  const boolQuery = {
+    must: {
+      query_string: {
+        fields,
+        query: keywords.length > 0 ? keywords.map(formatKeyword).join(' ') : '*',
+      },
+    },
   };
-  
-  console.log("Keywords:" + JSON.stringify(keywords));
 
-  var filter = [];
-  if (terms.length || termURIs.length) {
-    terms.forEach((t) => {
-      filter.push(nestedQuery({ "terms.label": t }));
-    });
+  console.log(`Keywords:${JSON.stringify(keywords)}`);
 
-    termURIs.forEach((t) => {
-      filter.push(nestedQuery({ "terms.uri": t }));
-    });
+  const filter = [];
+  if (terms.length > 0 || termURIs.length > 0) {
+    for (const t of terms) {
+      filter.push(nestedQuery({ 'terms.label': t }));
+    }
+
+    for (const t of termURIs) {
+      filter.push(nestedQuery({ 'terms.uri': t }));
+    }
   }
 
   if (refereed) {
-    filter.push({ "term": { "refereed": "Refereed" } });
+    filter.push({ term: { refereed: 'Refereed' } });
   }
 
   if (filter.length > 0) {
-    boolQuery["filter"] = filter;  
+    boolQuery['filter'] = filter;
   }
-  
-  var query = {
-    "bool": boolQuery
+
+  const query = {
+    bool: boolQuery,
   };
-  
+
   console.log(JSON.stringify(query));
-  
+
   return query;
 }
 
 /**
  * Returns the list of fields that should be targeted in an Elasticsearch query. All fields
  * are set to the default Elasticsearch boost value unless otherwise denoted by ^n.
- * 
+ *
  * @returns {array} Array of field names to target in a query
  */
 function queryFields() {
   return [
-          "contents", 
-          "title^2", 
-          "publisher", 
-          "author", 
-          "title_alt", 
-          "corp_author", 
-          "editor", 
-          "journal_title", 
-          "essential_ocean_variables", 
-          "sustainable_development_goals", 
-          "refereed",
-          "abstract^2",
-          "publication_status",
-          "current_status",
-          "relation_uri",
-          "language",
-          "bptype",
-          "relation_is_part_of_series",
-          "type",
-          "subjects_other",
-          "subjects_instrument_type",
-          "subjects_parameter_discipline",
-          "subjects_dm_processes",
-          "identifier_orcid",
-          "identifier_doi",
-          "maturity_level",
-          "notes",
-          "coverage_spatial"
-         ];
+    'contents',
+    'title^2',
+    'publisher',
+    'author',
+    'title_alt',
+    'corp_author',
+    'editor',
+    'journal_title',
+    'essential_ocean_variables',
+    'sustainable_development_goals',
+    'refereed',
+    'abstract^2',
+    'publication_status',
+    'current_status',
+    'relation_uri',
+    'language',
+    'bptype',
+    'relation_is_part_of_series',
+    'type',
+    'subjects_other',
+    'subjects_instrument_type',
+    'subjects_parameter_discipline',
+    'subjects_dm_processes',
+    'identifier_orcid',
+    'identifier_doi',
+    'maturity_level',
+    'notes',
+    'coverage_spatial',
+  ];
 }
 
 function nestedQuery(termPhrase) {
   return {
-    "nested": {
-      "path": "terms",
-      "query": {
-        "bool": {
-          "must": {
-            "match_phrase": termPhrase
-          }
-        }
-      }
-    }
+    nested: {
+      path: 'terms',
+      query: {
+        bool: {
+          must: {
+            match_phrase: termPhrase,
+          },
+        },
+      },
+    },
   };
 }
 
 function sortQuery(sortParams) {
-  var query = sortParams.map((s) => {
+  const query = sortParams.map((s) => {
     const keyAndDirection = s.split(':');
     if (keyAndDirection.length < 2) {
       return keyAndDirection[0];
-    } else {
-      var param = {};
-      param[keyAndDirection[0]] = keyAndDirection[1];
-      
-      return param;
     }
+    const param = {};
+    param[keyAndDirection[0]] = keyAndDirection[1];
+
+    return param;
   });
 
-  query.push("_score");
+  query.push('_score');
 
   return query;
 }
 
 function buildSynonymsQuery(term) {
-  var query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
+  const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
                PREFIX owl: <http://www.w3.org/2002/07/owl#> \
                PREFIX skos:<http://www.w3.org/2004/02/skos/core#> \
@@ -340,7 +365,7 @@ function buildSynonymsQuery(term) {
                   { \
                     SELECT DISTINCT ?xs WHERE { \
                       ?xs rdfs:label ?xl . \
-                      FILTER (?xl = '" + term + "'^^xsd:string) \
+                      FILTER (?xl = '${term}'^^xsd:string) \
                     } \
                   }\
                 } \
@@ -349,12 +374,12 @@ function buildSynonymsQuery(term) {
                   SELECT ?sameAsLabel \
                   WHERE { \
                     ?concept skos:prefLabel ?prefLabel . \
-                    FILTER (str(?prefLabel) = '" + term + "') \
+                    FILTER (str(?prefLabel) = '${term}') \
                     ?concept owl:sameAs ?sameAsConcept . \
                     ?sameAsConcept skos:prefLabel ?sameAsLabel . \
                   } \
                 } \
-              }";
+              }`;
 
   return query;
 }
@@ -362,30 +387,30 @@ function buildSynonymsQuery(term) {
 function buildSynonymsQueryOpts(query) {
   return {
     hostname: ontOpts.host,
-    path: ontOpts.path + "?query=" + encodeURIComponent(query),
+    path: `${ontOpts.path}?query=${encodeURIComponent(query)}`,
     port: ontOpts.port,
     headers: {
-      'Accept': 'application/json'
-    }
+      Accept: 'application/json',
+    },
   };
 }
 
 function getSynonyms(keywords) {
-  var promises = [];
-  keywords.forEach(function(k) {
-      const queryPromise = new Promise(function (resolve, reject) {
-        http.get(buildSynonymsQueryOpts(buildSynonymsQuery(k)), function(res) {
-        var body = '';
+  const promises = [];
+  for (const k of keywords) {
+    const queryPromise = new Promise((resolve, reject) => {
+      http.get(buildSynonymsQueryOpts(buildSynonymsQuery(k)), (res) => {
+        let body = '';
 
-        res.on('data', function(chunk) {
+        res.on('data', (chunk) => {
           body += chunk;
         });
 
-        res.on('error', function(err) {
+        res.on('error', (err) => {
           reject(err);
         });
 
-        res.on('end', function() {
+        res.on('end', () => {
           const synonyms = parseSynonymsResponse(JSON.parse(body));
           resolve(synonyms);
         });
@@ -393,30 +418,31 @@ function getSynonyms(keywords) {
     });
 
     promises.push(queryPromise);
-  });
+  }
 
   return Promise.all(promises);
 }
 
 function parseSynonymsResponse(body) {
-  var results = body.results.bindings, synonyms = [];
+  const results = body.results.bindings; const
+    synonyms = [];
   console.log(JSON.stringify(body));
-  results.forEach(function(r) {
-    if (r["annotatedPropertyLabel"] !== undefined) {
-      if (r["annotatedPropertyLabel"]["value"] === "has_exact_synonym" || r["annotatedPropertyLabel"]["value"] === "alternative_label") {
-        synonyms.push(r["annotatedTarget"]["value"]);
+  for (const r of results) {
+    if (r['annotatedPropertyLabel'] !== undefined) {
+      if (r['annotatedPropertyLabel']['value'] === 'has_exact_synonym' || r['annotatedPropertyLabel']['value'] === 'alternative_label') {
+        synonyms.push(r['annotatedTarget']['value']);
       }
-    } else if (r["sameAsLabel"] !== undefined) {
-      synonyms.push(r["sameAsLabel"]["value"]);
+    } else if (r['sameAsLabel'] !== undefined) {
+      synonyms.push(r['sameAsLabel']['value']);
     }
-  });
+  }
 
   return synonyms;
 }
 
 function responseHeaders() {
-  return { 
-    "Access-Control-Allow-Origin": "*", 
-    "Access-Control-Allow-Credentials": 'true'
+  return {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
   };
 }
