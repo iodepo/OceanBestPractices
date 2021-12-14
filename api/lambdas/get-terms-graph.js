ec8f20e31752c8eeb3ebe98d66377233401786ab
Marc Huffnagle <marc@element84.com>
2021-12-14 17:02:17 -0500

    autofix linter warnings in get-terms-graph

--- api/lambdas/get-terms-graph.js
+++ api/lambdas/get-terms-graph.js
@@ -12,7 +12,7 @@ const http = require('http');
 const ontology = {
   host: process.env.ONTOLOGY_STORE_HOST,
   port: process.env.ONTOLOGY_STORE_PORT,
-  path: "/sparql",
+  path: '/sparql',
 };
 
 /**
@@ -23,25 +23,33 @@ exports.handler = (event, context, callback) => {
   const queryParams = event.queryStringParameters;
 
   if (queryParams.termURI !== undefined) {
-    graphTermURI(queryParams.termURI, callback); 
+    graphTermURI(queryParams.termURI, callback);
   } else if (queryParams.term !== undefined) {
-    graphTerm(queryParams.term, callback);  
+    graphTerm(queryParams.term, callback);
   } else {
-    callback(null, { statusCode: 200, body: JSON.stringify({}), headers: responseHeaders() });
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({}),
+      headers: responseHeaders(),
+    });
   }
 };
 
 /**
  * Executes a semantic neighborhood query for a given term. This function first
  * exectures a search to get the term URI in order to execute the graph query.
- */ 
+ */
 function graphTerm(term, callback) {
   // Query the triple store to get the URI of the given term. That'll be used in
   // the actual graph query.
-  executeQuery(buildTermIRIQuery(term), function(err, results) {
+  executeQuery(buildTermIRIQuery(term), (err, results) => {
     if (err === null) {
-      if (results.length == 0 || results[0].s.value === undefined) {
-        callback(null, { statusCode: 200, body: JSON.stringify({}), headers: responseHeaders() });
+      if (results.length === 0 || results[0].s.value === undefined) {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({}),
+          headers: responseHeaders(),
+        });
       } else {
         const termIRI = results[0].s.value;
         graphTermURI(termIRI, callback);
@@ -49,7 +57,7 @@ function graphTerm(term, callback) {
     } else {
       respondWithError(err, callback);
     }
-  }); 
+  });
 }
 
 /**
@@ -60,13 +68,20 @@ function graphTermURI(termURI, callback) {
   // This is a hacky way to determine which query to use.
   // FIXME: This method should use metadata about the ontology (term URI) to determine
   // the ontology type and therefore which queries to use.
-  if (termURI.includes("purl.obolibrary.org") || termURI.includes("purl.unep.org")) {
-    executeQuery(buildSuperTermQuery(termURI), function(err, supers) {
+  if (termURI.includes('purl.obolibrary.org') || termURI.includes('purl.unep.org')) {
+    executeQuery(buildSuperTermQuery(termURI), (err, supers) => {
       if (err === null) {
-        executeQuery(buildUsesInOntologyQuery(termURI), function(err, uses) {
+        executeQuery(buildUsesInOntologyQuery(termURI), (err, uses) => {
           if (err === null) {
-            const graph = { "parents": parseSuperTermResponse(supers), "children": parseUsesInOntologyResponse(uses) };
-            callback(null, { statusCode: 200, body: JSON.stringify(graph), headers: responseHeaders() });  
+            const graph = {
+              parents: parseSuperTermResponse(supers),
+              children: parseUsesInOntologyResponse(uses),
+            };
+            callback(null, {
+              statusCode: 200,
+              body: JSON.stringify(graph),
+              headers: responseHeaders(),
+            });
           } else {
             respondWithError(err, callback);
           }
@@ -74,12 +89,16 @@ function graphTermURI(termURI, callback) {
       } else {
         respondWithError(err, callback);
       }
-    });  
+    });
   } else {
-    executeQuery(buildSKOSGraphQuery(termURI), function(err, relations) {
+    executeQuery(buildSKOSGraphQuery(termURI), (err, relations) => {
       if (err === null) {
         const graph = parseSKOSGraphResponse(relations);
-        callback(null, { statusCode: 200, body: JSON.stringify(graph), headers: responseHeaders() });
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(graph),
+          headers: responseHeaders(),
+        });
       } else {
         respondWithError(err, callback);
       }
@@ -93,11 +112,11 @@ function graphTermURI(termURI, callback) {
 function queryOpts(query) {
   return {
     hostname: ontology.host,
-    path: ontology.path + "?query=" + encodeURIComponent(query),
+    path: `${ontology.path}?query=${encodeURIComponent(query)}`,
     port: ontology.port,
     headers: {
-      'Accept': 'application/json'
-    }
+      Accept: 'application/json',
+    },
   };
 }
 
@@ -106,18 +125,18 @@ function queryOpts(query) {
  * neighborhood query.
  */
 function executeQuery(query, callback) {
-  http.get(queryOpts(query), function(res) {
-    var body = '';
+  http.get(queryOpts(query), (res) => {
+    let body = '';
 
-    res.on('data', function(chunk) {
+    res.on('data', (chunk) => {
       body += chunk;
     });
 
-    res.on('error', function(err) {
+    res.on('error', (err) => {
       callback(err, null);
     });
 
-    res.on('end', function() {
+    res.on('end', () => {
       callback(null, JSON.parse(body).results.bindings);
     });
   });
@@ -129,24 +148,24 @@ function executeQuery(query, callback) {
 function buildSKOSGraphQuery(termURI, callback) {
   const ontGraph = sourceGraphFor(termURI);
 
-  const query = "PREFIX dc:<http://purl.org/dc/elements/1.1/> \
+  const query = `PREFIX dc:<http://purl.org/dc/elements/1.1/> \
          PREFIX skos:<http://www.w3.org/2004/02/skos/core#> \
          PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
          SELECT ?narrower ?narrowerLabel ?broader ?broaderLabel ?related ?relatedLabel \
          WHERE {  \
            OPTIONAL {  \
-             <" + termURI + "> skos:broader ?broader .  \
+             <${termURI}> skos:broader ?broader .  \
              ?broader skos:prefLabel ?broaderLabel . \
            } \
            OPTIONAL {  \
-             <" + termURI + "> skos:narrower ?narrower .  \
+             <${termURI}> skos:narrower ?narrower .  \
              ?narrower skos:prefLabel ?narrowerLabel . \
            } \
            OPTIONAL {  \
-             <" + termURI + "> skos:related ?related .  \
+             <${termURI}> skos:related ?related .  \
              ?related skos:prefLabel ?relatedLabel . \
            } \
-         }";
+         }`;
 
   return query;
 }
@@ -156,14 +175,14 @@ function buildSKOSGraphQuery(termURI, callback) {
  * from a given term.
  */
 function buildTermIRIQuery(term) {
-  return "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
+  return `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
           PREFIX owl: <http://www.w3.org/2002/07/owl#> \
           SELECT DISTINCT ?s \
           WHERE { \
             ?s rdf:type owl:Class . \
             ?s rdfs:label ?label . \
-            FILTER ( str(?label) = '" + term + "' ) . \
-          }";
+            FILTER ( str(?label) = '${term}' ) . \
+          }`;
 }
 
 /**
@@ -172,17 +191,17 @@ function buildTermIRIQuery(term) {
  */
 function buildSuperTermQuery(termIRI) {
   const ontGraph = sourceGraphFor(termIRI);
-  return "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
+  return `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
           PREFIX owl: <http://www.w3.org/2002/07/owl#> \
           SELECT ?term ?superTerm ?superLabel ?relLabel ?ref ?refLabel \
-          FROM " + ontGraph + " \
+          FROM ${ontGraph} \
           WHERE { \
             ?term rdfs:subClassOf ?superTerm . \
             OPTIONAL { ?superTerm rdfs:label ?superLabel } \
             OPTIONAL { ?superTerm owl:onProperty ?rel . ?rel rdfs:label ?relLabel . } \
             OPTIONAL { ?superTerm owl:someValuesFrom ?ref . ?ref rdfs:label ?refLabel . } \
-            FILTER (?term = <" + termIRI + ">) \
-          }";
+            FILTER (?term = <${termIRI}>) \
+          }`;
 }
 
 /**
@@ -192,15 +211,15 @@ function buildSuperTermQuery(termIRI) {
 function buildUsesInOntologyQuery(termIRI) {
   const ontGraph = sourceGraphFor(termIRI);
 
-  var query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX owl: <http://www.w3.org/2002/07/owl#>";
-  query = query + " SELECT DISTINCT ?ref ?label ?rell";
-  query = query + " FROM " + ontGraph;
-  query = query + " WHERE { ?ref ?refp ?o . FILTER ( ?refp IN ( owl:equivalentClass, rdfs:subClassOf ) ) . OPTIONAL { ?ref rdfs:label ?label } . OPTIONAL { ?o owl:onProperty ?rel . ?rel rdfs:label ?rell } .";
-  query = query + " {";
-  query = query + " {";
-  query = query + " SELECT ?s ?p ?o FROM " + ontGraph;
-  query = query + " WHERE { ?o ?p ?s . FILTER ( ?p IN ( rdf:first, rdf:rest, owl:intersectionOf, owl:unionOf, owl:someValuesFrom, owl:hasValue, owl:allValuesFrom, owl:complementOf, owl:inverseOf, owl:onClass, owl:onProperty ) ) } }";
-  query = query + " FILTER ( ?s = <" + termIRI + "> ) } } ORDER BY ?label";
+  let query = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX owl: <http://www.w3.org/2002/07/owl#>';
+  query += ' SELECT DISTINCT ?ref ?label ?rell';
+  query = `${query} FROM ${ontGraph}`;
+  query += ' WHERE { ?ref ?refp ?o . FILTER ( ?refp IN ( owl:equivalentClass, rdfs:subClassOf ) ) . OPTIONAL { ?ref rdfs:label ?label } . OPTIONAL { ?o owl:onProperty ?rel . ?rel rdfs:label ?rell } .';
+  query += ' {';
+  query += ' {';
+  query = `${query} SELECT ?s ?p ?o FROM ${ontGraph}`;
+  query += ' WHERE { ?o ?p ?s . FILTER ( ?p IN ( rdf:first, rdf:rest, owl:intersectionOf, owl:unionOf, owl:someValuesFrom, owl:hasValue, owl:allValuesFrom, owl:complementOf, owl:inverseOf, owl:onClass, owl:onProperty ) ) } }';
+  query = `${query} FILTER ( ?s = <${termIRI}> ) } } ORDER BY ?label`;
 
   return query;
 }
@@ -212,42 +231,51 @@ function buildUsesInOntologyQuery(termIRI) {
  * vocabulary map to narrow, broader, and related annotated properties respectively.
  */
 function parseSKOSGraphResponse(results) {
-  var graph = {};
+  const graph = {};
 
-  results.forEach((r) => {
+  for (const r of results) {
     if (r.narrower !== undefined) {
-      if (graph["parents"] === undefined) {
-        graph["parents"] = {};
+      if (graph['parents'] === undefined) {
+        graph['parents'] = {};
       }
-      
-      if (graph["parents"]["narrower"] === undefined) {
-        graph["parents"]["narrower"] = [];
+
+      if (graph['parents']['narrower'] === undefined) {
+        graph['parents']['narrower'] = [];
       }
-      
-      graph["parents"]["narrower"].push({ "uri": r.narrower.value, "label": r.narrowerLabel.value });
+
+      graph['parents']['narrower'].push({
+        uri: r.narrower.value,
+        label: r.narrowerLabel.value,
+      });
     } else if (r.broader !== undefined) {
-      if (graph["children"] === undefined) {
-        graph["children"] = {};
+      if (graph['children'] === undefined) {
+        graph['children'] = {};
       }
-      
-      if (graph["children"]["broader"] === undefined) {
-        graph["children"]["broader"] = [];
+
+      if (graph['children']['broader'] === undefined) {
+        graph['children']['broader'] = [];
       }
-      
-      graph["children"]["broader"].push({ "uri": r.broader.value, "label": r.broaderLabel.value });
+
+      graph['children']['broader'].push({
+        uri: r.broader.value,
+        label: r.broaderLabel.value,
+      });
     } else if (r.related !== undefined) {
-      if (graph["siblings"] === undefined) {
-        graph["siblings"] = {};
+      if (graph['siblings'] === undefined) {
+        graph['siblings'] = {};
       }
-      
-      if (graph["siblings"]["related"] === undefined) {
-        graph["siblings"]["related"] = [];
+
+      if (graph['siblings']['related'] === undefined) {
+        graph['siblings']['related'] = [];
       }
-      
-      graph["siblings"]["related"].push({ "uri": r.related.value, "label": r.relatedLabel.value });
+
+      graph['siblings']['related'].push({
+        uri: r.related.value,
+        label: r.relatedLabel.value,
+      });
     }
-  });
-  
+  }
+
   return graph;
 }
 
@@ -255,14 +283,17 @@ function parseSKOSGraphResponse(results) {
  * Parses the result of the OWL "uses in ontology" query.
  */
 function parseUsesInOntologyResponse(results) {
-  var graph = {};
-  results.forEach((r) => {
+  const graph = {};
+  for (const r of results) {
     if (graph[r.rell.value] === undefined) {
       graph[r.rell.value] = [];
     }
 
-    graph[r.rell.value].push({ "uri": r.ref.value, "label": r.label.value });
-  });
+    graph[r.rell.value].push({
+      uri: r.ref.value,
+      label: r.label.value,
+    });
+  }
 
   return graph;
 }
@@ -271,30 +302,33 @@ function parseUsesInOntologyResponse(results) {
  * Parses the result of the OWL super class query.
  */
 function parseSuperTermResponse(results) {
-  var graph = {};
-  results.forEach((r) => {
-    
+  const graph = {};
+  for (const r of results) {
     // We can only return results that have a label in the searched ontology.
     // For those that have a node reference, check to make sure we have a label
     // for that node before trying to include it in the result set.
-    if (r.superTerm.value.match(/^nodeID/) && r.relLabel !== undefined) {
+    if (r.superTerm.value.startsWith('nodeID') && r.relLabel !== undefined) {
       if (graph[r.relLabel.value] === undefined) {
         graph[r.relLabel.value] = [];
       }
 
-      graph[r.relLabel.value].push({ "label": r.refLabel.value, "uri": r.ref.value });
+      graph[r.relLabel.value].push({
+        label: r.refLabel.value,
+        uri: r.ref.value,
+      });
     } else if (r.superLabel !== undefined && r.superTerm !== undefined) {
-      
-      // If there is no node reference, just assume the result is related as 
+      // If there is no node reference, just assume the result is related as
       // an "is a" relationship
-      if (graph["is a"] === undefined) {
-        graph["is a"] = [];
+      if (graph['is a'] === undefined) {
+        graph['is a'] = [];
       }
 
-      graph["is a"].push({ "label": r.superLabel.value, "uri": r.superTerm.value });
+      graph['is a'].push({
+        label: r.superLabel.value,
+        uri: r.superTerm.value,
+      });
     }
-
-  });
+  }
 
   return graph;
 }
@@ -302,39 +336,42 @@ function parseSuperTermResponse(results) {
 /**
  * FIXME: This method should instead be a lookup table in DynamoDB.
  * Helper function that maps a termURI to a source triple store graph.
- */ 
+ */
 function sourceGraphFor(termURI) {
-  const termID = termURI.substr(termURI.lastIndexOf('/') + 1);
-  if (termID.startsWith("CHEBI")) {
-    return "<http://purl.obolibrary.org/obo/chebi.owl>";
-  } else if (termID.startsWith("SDGIO")) {
-    return "<http://purl.unep.org/sdg/sdgio.owl>";
-  } else if (termID.startsWith("ENVO")) {
-    return "<http://purl.obolibrary.org/obo/envo.owl>";
-  } else if (termURI.includes("L05")) {
-    return "<http://vocab.nerc.ac.uk/collection/L05/current/>";
-  } else if (termURI.includes("L22")) {
-    return "<http://vocab.nerc.ac.uk/collection/L22/current/>";
-  } else if (termURI.includes("L06")) {
-    return "<http://vocab.nerc.ac.uk/collection/L06/current/>";
-  } else {
-    return null;
+  const termID = termURI.slice(termURI.lastIndexOf('/') + 1);
+  if (termID.startsWith('CHEBI')) {
+    return '<http://purl.obolibrary.org/obo/chebi.owl>';
+  } if (termID.startsWith('SDGIO')) {
+    return '<http://purl.unep.org/sdg/sdgio.owl>';
+  } if (termID.startsWith('ENVO')) {
+    return '<http://purl.obolibrary.org/obo/envo.owl>';
+  } if (termURI.includes('L05')) {
+    return '<http://vocab.nerc.ac.uk/collection/L05/current/>';
+  } if (termURI.includes('L22')) {
+    return '<http://vocab.nerc.ac.uk/collection/L22/current/>';
+  } if (termURI.includes('L06')) {
+    return '<http://vocab.nerc.ac.uk/collection/L06/current/>';
   }
+  return null;
 }
 
 /**
  * Helper function that sets a default error response.
  */
 function respondWithError(err, callback) {
-  callback(err, { statusCode: 500, body: JSON.stringify({'err': err}), headers: responseHeaders() });
+  callback(err, {
+    statusCode: 500,
+    body: JSON.stringify({ err }),
+    headers: responseHeaders(),
+  });
 }
 
 /**
  * Helper function that provides the default response headers.
  */
 function responseHeaders() {
-  return { 
-    "Access-Control-Allow-Origin": "*", 
-    "Access-Control-Allow-Credentials": 'true' 
+  return {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
   };
 }
