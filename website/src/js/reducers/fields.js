iodepo/OceanBestPractices

commit 699dc07dfb608ce4f89a7134f36446745ec23dc9

Small syntax to address PR feedback.

website/src/js/reducers/fields.js
@@ -100,10 +100,7 @@ const initialState = [
 ];
 
 export default (state = initialState, action) => {
-  let existingState = state;
-  if (!Array.isArray(existingState)) {
-    existingState = [];
-  }
+  const existingState = Array.isArray(initialState) ? initialState : [];
 
   // Set up a new array with new object instances
 

commit 6ebb6d9550de4cf850826fc753aca901a82a5309

fixes bug that resets fields dropdown state on every render introduced in 699dc07dfb608ce4f89a7134f36446745ec23dc9

website/src/js/reducers/fields.js
@@ -109,7 +109,7 @@ const initialState = [
 ];
 
 export default (state = initialState, action) => {
-  const existingState = Array.isArray(initialState) ? initialState : [];
+  const existingState = Array.isArray(state) ? state : [];
 
   // Set up a new array with new object instances
 