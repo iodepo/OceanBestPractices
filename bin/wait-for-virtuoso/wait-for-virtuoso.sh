33de8e7dadec6140198630a4e185a451f2b83c3c
Paul Pilone <paul@element84.com>
2022-06-01 12:21:49 -0400

    Tweaks virtuoso wait.

--- bin/wait-for-virtuoso/wait-for-virtuoso.sh
+++ bin/wait-for-virtuoso/wait-for-virtuoso.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 is_virtuoso_online () {
-  nc -v -z 127.0.0.1 1111 2>&1 |\
-    grep succeeded > /dev/null 2>&1
+  status=$(curl -s -X GET --write-out "%{http_code}" --output /dev/null "http://127.0.0.1:8890/sparql?query=SELECT%20DISTINCT%20%3Fg%20WHERE%20%7B%20GRAPH%20%3Fg%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D%20%7D")
+  [[ $status == 200 ]]
 }
 
 while ! is_virtuoso_online; do sleep 1; done
