5d0fbe3481e2362119c19cbe3752d13bcc81dbf2
Paul Pilone <paul@element84.com>
2022-06-01 14:08:24 -0400

    Testing digest auth for Virtuoso container

--- lib/graph-db-test.test.ts
+++ lib/graph-db-test.test.ts
@@ -1,6 +1,48 @@
 import got from 'got';
+import { ClientDigestAuth } from '@mreal/digest-auth';
 
-const sparqlUrl = 'http://localhost:8890/sparql';
+const sparqlUrl = 'http://localhost:8890/sparql-auth';
+
+export function createDigestClient(username: string, password: string) {
+  return got.extend({
+    hooks: {
+      afterResponse: [
+        (res, retry) => {
+          const { options } = res.request;
+          const digestHeader = res.headers['www-authenticate'];
+
+          if (!digestHeader) {
+            return res;
+          }
+
+          const incomingDigest = ClientDigestAuth.analyze(digestHeader);
+
+          console.log(incomingDigest);
+
+          const digest = ClientDigestAuth.generateProtectionAuth(
+            incomingDigest,
+            username,
+            password,
+            {
+              method: options.method,
+              uri: options.url.pathname,
+              counter: 1,
+            }
+          );
+
+          options.headers['authorization'] = digest.raw;
+
+          return retry(options);
+        },
+      ],
+      beforeRetry: [
+        (options, error, retryCount) => {
+          console.log(options.headers, error, retryCount);
+        },
+      ],
+    },
+  });
+}
 
 // This will probably go into a some sort of GraphDb helper. Again, this is all we have
 // for now and I just want a test that proves Docker/Github Actions are working as
@@ -21,7 +63,7 @@ describe('graph-db-test', () => {
   beforeAll(async () => {
     // Load ENVO
     console.log('🚀 ~ file: graph-db-test.test.ts ~ line 29 ~ beforeAll ~ Hello!');
-    const response = await got.post(sparqlUrl, {
+    const response = await createDigestClient('dba', 'obptest').post(sparqlUrl, {
       form: {
         query: 'LOAD <file:///usr/share/proj/envo.owl> INTO <https://purl.obolibrary.org/obo/envo.owl>',
       },
@@ -32,7 +74,7 @@ describe('graph-db-test', () => {
 
   afterAll(async () => {
     // Delete ENVO
-    await got.post(sparqlUrl, {
+    await createDigestClient('dba', 'obptest').post(sparqlUrl, {
       form: {
         query: 'CLEAR GRAPH <https://purl.obolibrary.org/obo/envo.owl>',
       },
@@ -41,7 +83,7 @@ describe('graph-db-test', () => {
   });
 
   test('should load ENVO', async () => {
-    const { body } = await got.post<GraphsResponse>(sparqlUrl, {
+    const { body } = await createDigestClient('dba', 'obptest').post<GraphsResponse>(sparqlUrl, {
       form: {
         query: 'SELECT  DISTINCT ?g WHERE  { GRAPH ?g {?s ?p ?o} } ORDER BY  ?g',
       },
